1f2226b3c33f03fa6f167cefb11b665fbe0d994f
shmaejp <shmaejp@gmail.com>
2021-08-14 15:25:48 +0900

    third commit

--- anataha_dareni_app/models.py
+++ anataha_dareni_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ModelFile(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to = 'documents/')
+    label = models.IntegerField('推論結果（ラベル）', blank=True, null=True)
+    proba = models.FloatField('信頼度（確率）', default=0.0)
+
+    def __str__(self):
+        return self.title
