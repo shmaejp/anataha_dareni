114c17663ff73671175f3d610d3166525eea27f0
shmaejp <shmaejp@gmail.com>
2021-08-17 20:26:33 +0900

    9th commit

--- anataha_dareni_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import ModelFile
-
-admin.site.register(ModelFile)
--- anataha_dareni_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnatahaDareniAppConfig(AppConfig):
-    name = 'anataha_dareni_app'
